e9c82c245dfcfefa21e38a32d5f8c67155c64cc1
sponrad <sponrad@gmail.com>
2024-09-08 18:22:29 -0700

    react-json-schema-form installed and basic demo working

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Form from '@rjsf/mui';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+
+const schema: RJSFSchema = {
+  title: 'Test form',
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+    },
+    age: {
+      type: 'number',
+    },
+  },
+};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +43,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        See the form down here
+      </p>
+      <Form schema={schema} validator={validator} />
     </>
   )
 }
