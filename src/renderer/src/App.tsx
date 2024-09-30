3c2270c6b7be604d48155dceebc3ca450195ff6a
sponrad <sponrad@gmail.com>
2024-09-30 16:10:28 -0700

    App: Filter form data keys removed when schema changes
    Nesting will be solved another day
    
    Fixes https://github.com/sponrad/json-editor-electron/issues/3

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -79,6 +79,15 @@ const App = () => {
                   // stringify+parse lets json schema errors stay in the editor for fixing
                   // rather than hard crashing the Form / App
                   setSchema(JSON.stringify(JSON.parse(newSchema)))
+
+                  // remove all form data fields that are not in the newSchema
+                  // TODO move out somewhere nicer
+                  const parsedFormData = JSON.parse(formData);
+                  const formDataKeys = Object.keys(parsedFormData);
+                  const schemaKeys = Object.keys(JSON.parse(newSchema)?.properties);
+                  const dataKeysNotInSchema = formDataKeys.filter(formDataKey => !schemaKeys.includes(formDataKey))
+                  dataKeysNotInSchema.forEach(deleteKey => delete parsedFormData[deleteKey])
+                  setFormData(JSON.stringify(parsedFormData));
                 }
               }}
               height="100%"
@@ -129,7 +138,6 @@ const App = () => {
                 formData={formData ? JSON.parse(formData) : undefined}
                 onChange={(onSubmit, id) => {
                   setFormData(toJson(onSubmit.formData));
-                  console.log(onSubmit.formData)
                   console.log(
                     onSubmit,
                     id,
